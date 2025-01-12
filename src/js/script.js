57244af722facf9187f1a418bed56716d8fd06df
kiartes <thaissouzabr4@gmail.com>
2025-01-12 11:54:40 +0000

    play

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+function open_fullscreen() {
+    var elem = document.getElementById("game-area");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('404 Page Loaded');
+    });
